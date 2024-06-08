3a8d929282cd5aea27828e34053c819e4d64f4f5
Artem Musalitin <musalitin.a.a@gmail.com>
2024-06-08 13:32:49 +0200

    Use htmx

---
+++ README.md
@@ -0,0 +1,15 @@
+# GO + HTMX
+
+## Requirements
+
+- Go
+- (optional) https://github.com/air-verse/air for hot reload
+
+## Start server
+```bash
+air
+```
+or just
+```bash
+go run ./cmd/main.go
+```
--- cmd/main.go
+++ cmd/main.go
@@ -22,20 +22,110 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type Data struct {
+	Contacts []Contact
+}
+
+func NewData() *Data {
+	return &Data{
+		Contacts: []Contact{
+			{
+				Name:  "John Doe",
+				Email: "john.doe@gmail.com",
+			},
+			{
+				Name:  "Jane Doe",
+				Email: "jain.doe@gmail.com",
+			},
+		},
+	}
+}
+
+type FormData struct {
+	Errors map[string]string
+	Values map[string]string
+}
+
+func NewFormData() FormData {
+	return FormData{
+		Errors: map[string]string{},
+		Values: map[string]string{},
+	}
+}
+
+type PageData struct {
+	Data Data
+	Form FormData
+}
+
+func NewContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+func NewPageData(data Data, form FormData) PageData {
+	return PageData{
+		Data: data,
+		Form: form,
+	}
+}
+
+func contactExists(contacts []Contact, email string) bool {
+	for _, c := range contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	e := echo.New()
-	count := Count{Count: 0}
-
 	e.Renderer = newTemplate()
 	e.Use(middleware.Logger())
 
+	data := NewData()
+
 	e.GET("/", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "index.html", count)
+		return c.Render(200, "index.html", NewPageData(*data, NewFormData()))
+	})
+
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if contactExists(data.Contacts, email) {
+			formData := FormData{
+				Errors: map[string]string{
+					"email": "Email already exists",
+				},
+				Values: map[string]string{
+					"name": name,
+					"email": email,
+				},
+			}
+
+			return c.Render(422, "contact-form.html", formData)
+		}
+
+		contact := NewContact(name, email)
+		data.Contacts = append(data.Contacts, contact)
+
+		formData := NewFormData()
+		err := c.Render(200, "contact-form.html", formData)
+
+		if err != nil {
+			return err
+		}
+
+		return c.Render(200, "oob-contact.html", contact)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
